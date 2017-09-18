63a272ad38127cae169fda6efe8b8e1dadd206a3
malenge254 <techiechic12.34@gmail.com>
2017-09-18 10:43:14 +0000

    first commit

---
+++ contacts.py
@@ -0,0 +1,30 @@
+class Contact:
+    
+    def __init__(self,cname,phoneno,email,website,bday):
+        
+        self.cname = cname
+        self.phoneno = phoneno
+        self.email = email
+        self.website = website
+        self.bday = bday
+
+
+cname = input('Name: ')
+phoneno = input('Phone: ')
+email = input('Email: ')
+website = input('Website: ')
+bday = input("bday: ")
+
+newContact = Contact(cname,phoneno,email,website,bday)
+print(newContact.cname)
+print(newContact.phoneno)
+print(newContact.email)
+print(newContact.website)
+print(newContact.bday)
+
+
+
+    
+
+    
+
